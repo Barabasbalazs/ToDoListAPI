7c5d12a98b3acd5487f201817a1a4333062d0142
Barabas Balazs <barabas.balazska@gmail.com>
2023-02-16 14:06:21 +0200

    ITL-10-Structure of auth system, routes and controller added

---
+++ src/routes/auth/auth-controller.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export const authController = {
+  login: async (req: Request, res: Response, next: NextFunction) => {},
+  logout: async (req: Request, res: Response, next: NextFunction) => {},
+  register: async (req: Request, res: Response, next: NextFunction) => {},
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import todoRouter from "./todos/todo-router";
+import authRouter from "./auth/auth-router";
 
 const router = Router();
 
 router.use("/todos", todoRouter);
+router.use("/auth", authRouter);
 
 export default router;
